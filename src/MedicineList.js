99a62d725ec34a7fd6d5b888197280b371a8a389
krisnakris <krisna.post@gmail.com>
2021-04-16 15:44:38 +0700

    tabel untuk list dokter, obat, dan appointment

--- src/MedicineList.js
+++ src/MedicineList.js
@@ -7,7 +7,6 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles({
   table: {
@@ -23,19 +22,16 @@ function createData(username, name, specialty, availableDay, availableTime) {
 }
 
 const rows = [
-  createData('Mark', "Otto", "Orthopedy", "Monday", "10.00-15.00" ),
+  createData('Marks', "Otto", "Orthopedy", "Monday", "10.00-15.00" ),
   createData('Jacob', "Thornton", "Skin", "Monday", "15.00-18.00"),
   createData('Larry', "Larry", "eye", "Saturday", "08.00-12.00"),
   createData('Larry', "Larry", "eye", "Sunday", "08.00-12.00"),
 ];
 
-export default function Home() {
+export default function MedicineList() {
   const classes = useStyles();
 
   return (
-    <Container>
-    <h3> List of all doctors </h3>
-
     <TableContainer component={Paper}>
       <Table className={ classes.table } aria-label="simple table">
         <TableHead className = { classes.header }>
@@ -62,6 +58,5 @@ export default function Home() {
         </TableBody>
       </Table>
     </TableContainer>
-    </Container>
   );
 }
