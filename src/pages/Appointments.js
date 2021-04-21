9e4a67539a9d59b7de8fec2dbfe564f2826d740a
krisnakris <krisna.post@gmail.com>
2021-04-21 21:06:08 +0700

    fixing sorting date

--- src/pages/Appointments.js
+++ src/pages/Appointments.js
@@ -155,7 +155,7 @@ export default function Appointments () {
   let rowsToShow = filter === "" ? rows : rows.filter(row => row.patient.name.toLowerCase().includes(filter.toLowerCase()));
 
   rowsToShow = rowsToShow.sort(function (a, b) {
-    return new Date(b.appointmentDate) - new Date(a.appointmentDate);
+    return new Date(a.appointmentDate) - new Date(b.appointmentDate);
   })
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, rowsToShow.length - page * rowsPerPage);
